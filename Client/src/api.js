ca32ab985384e82ef7e3c634679e770be7f9b4f5
Nilusha01 <senadeeramnr@uom.lk>
2026-08-02 03:26:02 +0530

    Fix API URL missing /api/bookings path

--- Client/src/api.js
+++ Client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://time-slot-bookong.onrender.com ';
+const API_URL = 'https://time-slot-bookong.onrender.com/api/bookings';
 
 export const getBookings = () => axios.get(API_URL);
 export const createBooking = (data) => axios.post(API_URL, data);
